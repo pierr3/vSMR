c506051792f33ff5c38f175d71a73f0ec4640558
DrFreas <51025240+DrFreas@users.noreply.github.com>
2019-12-26 17:54:53 +0100

    Reply Unable, but better and some polishing
    When an automatically "UNABLE" is triggered, it will actually not flag a Request and when a Telex-Message has been sent, it will now delete the original Telex that has been sent.

--- vSMR/SMRPlugin.cpp
+++ vSMR/SMRPlugin.cpp
@@ -110,6 +110,9 @@ void sendDatalinkMessage(void * arg) {
 	if (startsWith("ok", raw.c_str())) {
 		if (PendingMessages.find(DatalinkToSend.callsign) != PendingMessages.end())
 			PendingMessages.erase(DatalinkToSend.callsign);
+		if (std::find(AircraftMessage.begin(), AircraftMessage.end(), DatalinkToSend.callsign.c_str()) != AircraftMessage.end()) {
+			AircraftMessage.erase(std::remove(AircraftMessage.begin(), AircraftMessage.end(), DatalinkToSend.callsign.c_str()), AircraftMessage.end());
+		}
 		AircraftMessageSent.push_back(DatalinkToSend.callsign.c_str());
 	}
 };
@@ -161,11 +164,13 @@ void pollMessages(void * arg) {
 					ttype = "CPDLC";
 					tdest = DatalinkToSend.callsign;
 					_beginthread(sendDatalinkMessage, 0, NULL);
-				} else if (PlaySoundClr) {
-					AFX_MANAGE_STATE(AfxGetStaticModuleState());
-					PlaySound(MAKEINTRESOURCE(IDR_WAVE1), AfxGetInstanceHandle(), SND_RESOURCE | SND_ASYNC);
+				} else {
+					if (PlaySoundClr) {
+						AFX_MANAGE_STATE(AfxGetStaticModuleState());
+						PlaySound(MAKEINTRESOURCE(IDR_WAVE1), AfxGetInstanceHandle(), SND_RESOURCE | SND_ASYNC);
+					}
+					AircraftDemandingClearance.push_back(message.from);
 				}
-				AircraftDemandingClearance.push_back(message.from);
 			}
 			else if (message.message.find("WILCO") != std::string::npos || message.message.find("ROGER") != std::string::npos || message.message.find("RGR") != std::string::npos) {
 				if (std::find(AircraftMessageSent.begin(), AircraftMessageSent.end(), message.from) != AircraftMessageSent.end()) {
